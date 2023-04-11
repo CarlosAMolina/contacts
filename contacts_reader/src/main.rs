65c136674106ca46fb8f324449865e6524838c4c
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-04-11 22:29:11 +0200

    Add new fields

--- contacts_reader/src/main.rs
+++ contacts_reader/src/main.rs
@@ -4,15 +4,51 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 struct Contact {
+    id: usize,
     name: String,
+    surname: String,
+    nickname: String,
     phone: usize,
+    phone_description: String,
+    category: String,
+    address: String,
+    email: String,
+    url: String,
+    facebook_url: String,
+    twitter_handle: String,
+    note: String,
 }
 
 impl Contact {
-    fn new(name: &str, phone: &str) -> Self {
+    fn new(
+        id: &str,
+        name: &str,
+        surname: &str,
+        nickname: &str,
+        phone: &str,
+        phone_description: &str,
+        category: &str,
+        address: &str,
+        email: &str,
+        url: &str,
+        facebook_url: &str,
+        twitter_handle: &str,
+        note: &str,
+    ) -> Self {
         Contact {
+            id: id.to_string().parse::<usize>().unwrap(),
             name: name.to_string(),
+            surname: surname.to_string(),
+            nickname: nickname.to_string(),
             phone: phone.to_string().parse::<usize>().unwrap(),
+            phone_description: phone_description.to_string(),
+            category: category.to_string(),
+            address: address.to_string(),
+            email: email.to_string(),
+            url: url.to_string(),
+            facebook_url: facebook_url.to_string(),
+            twitter_handle: twitter_handle.to_string(),
+            note: note.to_string(),
         }
     }
 }
@@ -38,8 +74,22 @@ fn run() -> Result<(), Box<dyn Error>> {
             .iter()
             .any(|field| field.to_lowercase().contains(&query.to_lowercase()))
         {
-            let contact = Contact::new(&record[0], &record[1]);
-            println!("{:?} - {:?}", contact.phone, contact.name);
+            let contact = Contact::new(
+                &record[0],
+                &record[1],
+                &record[2],
+                &record[3],
+                &record[4],
+                &record[5],
+                &record[6],
+                &record[7],
+                &record[8],
+                &record[9],
+                &record[10],
+                &record[11],
+                &record[12],
+                );
+            println!("{:?} {:?} - {:?} {:?} {:?} {:?}", contact.phone, contact.phone_description, contact.name, contact.surname, contact.nickname, contact.category);
         }
     }
     Ok(())

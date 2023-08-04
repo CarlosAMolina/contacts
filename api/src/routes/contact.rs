1336ff9f1b1269839f7672d6aba3889a4823dc8d
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-08-04 20:50:14 +0200

    Partial commit get contact from all data

--- api/src/routes/contact.rs
+++ api/src/routes/contact.rs
@@ -3,12 +3,58 @@ use std::collections::HashMap;
 use std::println;
 
 use crate::store::Store;
+use crate::types::contact::{AllData, Phone, Contact};
 use crate::types::pagination::extract_pagination;
 
 pub async fn get_contact_by_id(id: i32, store: Store) -> Result<impl warp::Reply, warp::Rejection> {
     match store.get_all_data_by_id(id).await {
-        Ok(res) => Ok(warp::reply::json(&res)),
+        Ok(all_data) => Ok(warp::reply::json(&get_contact_from_all_data(all_data))),
         Err(e) => return Err(warp::reject::custom(e)),
+    }
+}
+
+// TODO not use clone
+// TODO create the vectors in a better way
+fn get_contact_from_all_data(all_data_vec: Vec<AllData>) -> Contact {
+    let mut addresses = vec![];
+    let mut categories = vec![];
+    let mut emails = vec![];
+    let mut facebook_urls = vec![];
+    let mut instagram_handles = vec![];
+    let mut nicknames = vec![];
+    let mut phones = vec![];
+    let mut twitter_handles = vec![];
+    let mut urls = vec![];
+    let note = all_data_vec[0].note.clone();
+    let user_id = all_data_vec[0].user_id.clone();
+    let user_name = all_data_vec[0].user_name.clone();
+    let user_surname = all_data_vec[0].user_surname.clone();
+    // TODO fix phone, only the 1st value is used.
+    for all_data in all_data_vec.iter().cloned() {
+        if let Some(value) = all_data.address  { if !addresses.contains(&value) {addresses.push(value);}}
+        if let Some(value) = all_data.category { if !categories.contains(&value) {categories.push(value); }}
+        if let Some(value) = all_data.email    { if !emails.contains(&value) {emails.push(value); }}
+        if let Some(value) = all_data.facebook_url { if !facebook_urls.contains(&value) {facebook_urls.push(value); }}
+        if let Some(value) = all_data.instagram_handle { if !instagram_handles.contains(&value) {instagram_handles.push(value); }}
+        if let Some(value) = all_data.nickname { if !nicknames.contains(&value) {nicknames.push(value); }}
+        if let Some(value) = all_data.phone { if phones.is_empty() {phones.push(Phone{ value, description: all_data.phone_description}); }  }
+        if let Some(value) = all_data.twitter_handle { if !twitter_handles.contains(&value) {twitter_handles.push(value); }}
+        if let Some(value) = all_data.url { if !urls.contains(&value) {urls.push(value); }}
+    }
+    Contact {
+        user_id,
+        user_name,
+        user_surname,
+        nicknames,
+        phones,
+        categories,
+        addresses,
+        emails,
+        urls,
+        facebook_urls,
+        twitter_handles,
+        instagram_handles,
+        note,
     }
 }
 
@@ -39,5 +85,67 @@ pub async fn get_contacts(
             Ok(res) => Ok(warp::reply::json(&res)),
             Err(e) => return Err(warp::reject::custom(e)),
         }
+    }
+}
+
+
+#[cfg(test)]
+mod config_tests {
+    use super::*;
+    use crate::types::contact;
+
+    #[test]
+    fn get_contact_from_all_data_manages_duplicates_correctly() {
+        let all_data_vec = vec![
+            contact::AllData {
+                user_id: contact::UserId(1),
+                user_name: Some("John".to_string()),
+                user_surname: Some("Doe".to_string()),
+                nickname: Some("Johnny".to_string()),
+                phone: Some(666666666),
+                phone_description: Some("Work".to_string()),
+                category: Some("Friends".to_string()),
+                address: Some("C/ 123".to_string()),
+                email: Some("john@doe.com".to_string()),
+                url: Some("john.com".to_string()),
+                facebook_url: Some("facebook/John".to_string()),
+                twitter_handle: Some("JohnT".to_string()),
+                instagram_handle: Some("JohnI".to_string()),
+                note: Some("Foo bar".to_string()),
+            },
+            contact::AllData {
+                user_id: contact::UserId(1),
+                user_name: Some("John".to_string()),
+                user_surname: Some("Doe".to_string()),
+                nickname: Some("Johnny".to_string()),
+                phone: Some(666666666),
+                phone_description: Some("Work".to_string()),
+                category: Some("Friends".to_string()),
+                address: Some("C/ 123".to_string()),
+                email: Some("john@doe.com".to_string()),
+                url: Some("john.com".to_string()),
+                facebook_url: Some("facebook/John".to_string()),
+                twitter_handle: Some("JohnT".to_string()),
+                instagram_handle: Some("JohnI".to_string()),
+                note: Some("Foo bar".to_string()),
+            }
+        ];
+        let expected = contact::Contact {
+            user_id: contact::UserId(1),
+            user_name: Some("John".to_string()),
+            user_surname: Some("Doe".to_string()),
+            nicknames: vec![("Johnny".to_string())],
+            phones: vec![contact::Phone{value: 666666666, description: Some("Work".to_string())}],
+            categories: vec!["Friends".to_string()],
+            addresses: vec!["C/ 123".to_string()],
+            emails: vec!["john@doe.com".to_string()],
+            urls: vec!["john.com".to_string()],
+            facebook_urls: vec!["facebook/John".to_string()],
+            twitter_handles: vec!["JohnT".to_string()],
+            instagram_handles: vec!["JohnI".to_string()],
+            note: Some("Foo bar".to_string()),
+        };
+        let result = get_contact_from_all_data(all_data_vec);
+        assert_eq!(expected, result);
     }
 }
--- api/src/types/contact.rs
+++ api/src/types/contact.rs
@@ -1,6 +1,6 @@
 use serde::Serialize;
 
-#[derive(Debug, Serialize)]
+#[derive(Clone, Debug, Serialize)]
 pub struct AllData {
     pub user_id: UserId,
     pub user_name: Option<String>,
@@ -18,5 +18,28 @@ pub struct AllData {
     pub note: Option<String>,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, PartialEq, Serialize)]
+pub struct Contact {
+    pub user_id: UserId,
+    pub user_name: Option<String>,
+    pub user_surname: Option<String>,
+    pub nicknames: Vec<String>,
+    pub phones: Vec<Phone>,
+    pub categories: Vec<String>,
+    pub addresses: Vec<String>,
+    pub emails: Vec<String>,
+    pub urls: Vec<String>,
+    pub facebook_urls: Vec<String>,
+    pub twitter_handles: Vec<String>,
+    pub instagram_handles: Vec<String>,
+    pub note: Option<String>,
+}
+
+#[derive(Debug, PartialEq, Serialize)]
+pub struct Phone {
+    pub value: i64,
+    pub description: Option<String>,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize)]
 pub struct UserId(pub i32);

3a27725b5cac8f0de63d80a9c91a29a637b2ab20
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-08-05 12:32:19 +0200

    Apply cargo fmt

--- api/src/routes/contact.rs
+++ api/src/routes/contact.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use std::println;
 
 use crate::store::Store;
-use crate::types::contact::{AllData, Phone, Contact};
+use crate::types::contact::{AllData, Contact, Phone};
 use crate::types::pagination::extract_pagination;
 
 pub async fn get_contact_by_id(id: i32, store: Store) -> Result<impl warp::Reply, warp::Rejection> {
@@ -15,9 +15,18 @@ pub async fn get_contact_by_id(id: i32, store: Store) -> Result<impl warp::Reply
 
 // TODO not use clone
 fn get_contact_from_all_data(all_data_vec: Vec<AllData>) -> Contact {
-    let mut all_data_phone_unique: Vec<_> = all_data_vec.iter().filter(|row| row.phone.is_some()).collect();
+    let mut all_data_phone_unique: Vec<_> = all_data_vec
+        .iter()
+        .filter(|row| row.phone.is_some())
+        .collect();
     all_data_phone_unique.dedup_by(|a, b| a.phone == b.phone);
-    let phones: Vec<Phone> = all_data_phone_unique.iter().map(|row| Phone{value: row.phone.unwrap(), description: row.phone_description.clone()}).collect();
+    let phones: Vec<Phone> = all_data_phone_unique
+        .iter()
+        .map(|row| Phone {
+            value: row.phone.unwrap(),
+            description: row.phone_description.clone(),
+        })
+        .collect();
     let mut addresses = vec![];
     let mut categories = vec![];
     let mut emails = vec![];
@@ -31,14 +40,30 @@ fn get_contact_from_all_data(all_data_vec: Vec<AllData>) -> Contact {
     let user_name = all_data_vec[0].user_name.clone();
     let user_surname = all_data_vec[0].user_surname.clone();
     for all_data in all_data_vec.iter().cloned() {
-        if let Some(value) = all_data.address  { push_to_vector_if_new(&mut addresses, value);}
-        if let Some(value) = all_data.category { push_to_vector_if_new(&mut categories, value);}
-        if let Some(value) = all_data.email    { push_to_vector_if_new(&mut emails, value);}
-        if let Some(value) = all_data.facebook_url { push_to_vector_if_new(&mut facebook_urls, value);}
-        if let Some(value) = all_data.instagram_handle { push_to_vector_if_new(&mut instagram_handles, value);}
-        if let Some(value) = all_data.nickname { push_to_vector_if_new(&mut nicknames, value);}
-        if let Some(value) = all_data.twitter_handle { push_to_vector_if_new(&mut twitter_handles, value);}
-        if let Some(value) = all_data.url { push_to_vector_if_new(&mut urls, value);}
+        if let Some(value) = all_data.address {
+            push_to_vector_if_new(&mut addresses, value);
+        }
+        if let Some(value) = all_data.category {
+            push_to_vector_if_new(&mut categories, value);
+        }
+        if let Some(value) = all_data.email {
+            push_to_vector_if_new(&mut emails, value);
+        }
+        if let Some(value) = all_data.facebook_url {
+            push_to_vector_if_new(&mut facebook_urls, value);
+        }
+        if let Some(value) = all_data.instagram_handle {
+            push_to_vector_if_new(&mut instagram_handles, value);
+        }
+        if let Some(value) = all_data.nickname {
+            push_to_vector_if_new(&mut nicknames, value);
+        }
+        if let Some(value) = all_data.twitter_handle {
+            push_to_vector_if_new(&mut twitter_handles, value);
+        }
+        if let Some(value) = all_data.url {
+            push_to_vector_if_new(&mut urls, value);
+        }
     }
     Contact {
         user_id,
@@ -92,7 +117,6 @@ pub async fn get_contacts(
         }
     }
 }
-
 
 #[cfg(test)]
 mod config_tests {
@@ -152,7 +176,7 @@ mod config_tests {
                 twitter_handle: None,
                 instagram_handle: Some("JohnI".to_string()),
                 note: Some("Foo bar".to_string()),
-            }
+            },
         ];
         let expected = contact::Contact {
             user_id: contact::UserId(1),
@@ -160,8 +184,14 @@ mod config_tests {
             user_surname: Some("Doe".to_string()),
             nicknames: vec![("Johnny".to_string())],
             phones: vec![
-                contact::Phone{value: 666666666, description: Some("Work".to_string())},
-                contact::Phone{value: 666666661, description: None},
+                contact::Phone {
+                    value: 666666666,
+                    description: Some("Work".to_string()),
+                },
+                contact::Phone {
+                    value: 666666661,
+                    description: None,
+                },
             ],
             categories: vec!["Friends".to_string()],
             addresses: vec!["C/ 123".to_string()],

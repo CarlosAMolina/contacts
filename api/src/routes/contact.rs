e56cc3a660fc86328fe9229da4240166074d77e7
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-08-05 12:05:00 +0200

    Add get unique phones

--- api/src/routes/contact.rs
+++ api/src/routes/contact.rs
@@ -16,20 +16,23 @@ pub async fn get_contact_by_id(id: i32, store: Store) -> Result<impl warp::Reply
 // TODO not use clone
 // TODO create the vectors in a better way: https://stackoverflow.com/questions/64819025/is-there-a-simple-way-remove-duplicate-elements-from-an-array
 fn get_contact_from_all_data(all_data_vec: Vec<AllData>) -> Contact {
+
+    let mut all_data_phone_unique: Vec<_> = all_data_vec.iter().filter(|row| row.phone.is_some()).collect();
+    all_data_phone_unique.dedup_by(|a, b| a.phone == b.phone);
+    let phones: Vec<Phone> = all_data_phone_unique.iter().map(|row| Phone{value: row.phone.unwrap(), description: row.phone_description.clone()}).collect();
+
     let mut addresses = vec![];
     let mut categories = vec![];
     let mut emails = vec![];
     let mut facebook_urls = vec![];
     let mut instagram_handles = vec![];
     let mut nicknames = vec![];
-    let mut phones = vec![];
     let mut twitter_handles = vec![];
     let mut urls = vec![];
     let note = all_data_vec[0].note.clone();
     let user_id = all_data_vec[0].user_id.clone();
     let user_name = all_data_vec[0].user_name.clone();
     let user_surname = all_data_vec[0].user_surname.clone();
-    // TODO fix phone, only the 1st value is used.
     for all_data in all_data_vec.iter().cloned() {
         if let Some(value) = all_data.address  { if !addresses.contains(&value) {addresses.push(value);}}
         if let Some(value) = all_data.category { if !categories.contains(&value) {categories.push(value); }}
@@ -37,7 +40,6 @@ fn get_contact_from_all_data(all_data_vec: Vec<AllData>) -> Contact {
         if let Some(value) = all_data.facebook_url { if !facebook_urls.contains(&value) {facebook_urls.push(value); }}
         if let Some(value) = all_data.instagram_handle { if !instagram_handles.contains(&value) {instagram_handles.push(value); }}
         if let Some(value) = all_data.nickname { if !nicknames.contains(&value) {nicknames.push(value); }}
-        if let Some(value) = all_data.phone { if phones.is_empty() {phones.push(Phone{ value, description: all_data.phone_description}); }  }
         if let Some(value) = all_data.twitter_handle { if !twitter_handles.contains(&value) {twitter_handles.push(value); }}
         if let Some(value) = all_data.url { if !urls.contains(&value) {urls.push(value); }}
     }
@@ -96,6 +98,7 @@ mod config_tests {
 
     #[test]
     fn get_contact_from_all_data_manages_duplicates_correctly() {
+        // TODO test empty phone
         let all_data_vec = vec![
             contact::AllData {
                 user_id: contact::UserId(1),
@@ -113,13 +116,31 @@ mod config_tests {
                 instagram_handle: Some("JohnI".to_string()),
                 note: Some("Foo bar".to_string()),
             },
+            // Empty phone
             contact::AllData {
                 user_id: contact::UserId(1),
                 user_name: Some("John".to_string()),
                 user_surname: Some("Doe".to_string()),
                 nickname: Some("Johnny".to_string()),
-                phone: Some(666666666),
-                phone_description: Some("Work".to_string()),
+                phone: None,
+                phone_description: None,
+                category: Some("Friends".to_string()),
+                address: Some("C/ 123".to_string()),
+                email: Some("john@doe.com".to_string()),
+                url: Some("john.com".to_string()),
+                facebook_url: Some("facebook/John".to_string()),
+                twitter_handle: Some("JohnT".to_string()),
+                instagram_handle: Some("JohnI".to_string()),
+                note: Some("Foo bar".to_string()),
+            },
+            // Empty phone descrition
+            contact::AllData {
+                user_id: contact::UserId(1),
+                user_name: Some("John".to_string()),
+                user_surname: Some("Doe".to_string()),
+                nickname: Some("Johnny".to_string()),
+                phone: Some(666666661),
+                phone_description: None,
                 category: Some("Friends".to_string()),
                 address: Some("C/ 123".to_string()),
                 email: Some("john@doe.com".to_string()),
@@ -135,7 +156,10 @@ mod config_tests {
             user_name: Some("John".to_string()),
             user_surname: Some("Doe".to_string()),
             nicknames: vec![("Johnny".to_string())],
-            phones: vec![contact::Phone{value: 666666666, description: Some("Work".to_string())}],
+            phones: vec![
+                contact::Phone{value: 666666666, description: Some("Work".to_string())},
+                contact::Phone{value: 666666661, description: None},
+            ],
             categories: vec!["Friends".to_string()],
             addresses: vec!["C/ 123".to_string()],
             emails: vec!["john@doe.com".to_string()],

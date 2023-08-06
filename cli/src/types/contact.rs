efe73f7b5cde920490cefa3112556a6695658c78
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-08-06 03:16:15 +0200

    Drop unrequired type

--- cli/src/types/contact.rs
+++ cli/src/types/contact.rs
@@ -1,23 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct AllData {
-    pub user_id: i32,
-    pub user_name: Option<String>,
-    pub user_surname: Option<String>,
-    pub nickname: Option<String>,
-    pub phone: Option<i64>,
-    pub phone_description: Option<String>,
-    pub category: Option<String>,
-    pub address: Option<String>,
-    pub email: Option<String>,
-    pub url: Option<String>,
-    pub facebook_url: Option<String>,
-    pub twitter_handle: Option<String>,
-    pub instagram_handle: Option<String>,
-    pub note: Option<String>,
-}
-
 #[derive(Debug, Deserialize, PartialEq, Serialize)]
 pub struct Contact {
     pub user_id: i32,

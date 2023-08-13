93a107a39deafd9bd7790f931ac6f0176c624a11
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-08-13 10:14:02 +0200

    Refactor extract function build_routes

--- api/src/main.rs
+++ api/src/main.rs
@@ -3,7 +3,7 @@
 use config::Config;
 use tracing_appender::rolling::{RollingFileAppender, Rotation};
 use tracing_subscriber::fmt::writer::MakeWriterExt;
-use warp::{http::Method, Filter};
+use warp::{http::Method, Filter, Reply};
 
 use handle_errors::return_error;
 
@@ -114,32 +114,12 @@ pub async fn setup_store(config: &ConfigArgs) -> store::Store {
     // TODO use .map_err(|e| handle_errors::Error::DatabaseQueryError(e))?;
 }
 
-#[tokio::main]
-async fn main() {
-    let config = ConfigArgs::new();
-    let store = setup_store(&config).await;
-
-    let log_filter = format!(
-        // TODO "handle_errors={},api={},warp={}",
-        "api={},warp={}", // TODO
-        config.log_level_api, config.log_level_warp
-    );
-    let logfile =
-        RollingFileAppender::new(Rotation::DAILY, config.log_path_name, config.log_file_name);
-    let (non_blocking_logfile, _guard_logfile) = tracing_appender::non_blocking(logfile);
-    let (non_blocking_stdout, _guard_stdout) = tracing_appender::non_blocking(std::io::stdout());
-    tracing_subscriber::fmt()
-        .with_env_filter(log_filter)
-        .with_writer(non_blocking_logfile.and(non_blocking_stdout))
-        //.with_span_events(tracing_subscriber::fmt::format::FmtSpan::CLOSE)
-        .init();
-
+async fn build_routes(store: store::Store) -> impl Filter<Extract = impl Reply> + Clone {
     let store_filter = warp::any().map(move || store.clone());
     let cors = warp::cors()
         .allow_any_origin()
         .allow_header("content-type")
         .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
-
     let get_contacts = warp::get()
         .and(warp::path("contacts"))
         .and(warp::path::end())
@@ -160,7 +140,6 @@ async fn main() {
                   id = %info_values.id,
             )
         }));
-
     let get_contact_by_id = warp::get()
         .and(warp::path("contacts"))
         .and(warp::path::param::<i32>())
@@ -181,12 +160,34 @@ async fn main() {
                   id = %info_values.id,
             )
         }));
-
-    let routes = get_contacts
+    get_contacts
         .or(get_contact_by_id)
         .with(cors)
         .with(warp::trace::request())
-        .recover(return_error);
+        .recover(return_error)
+}
+
+#[tokio::main]
+async fn main() {
+    let config = ConfigArgs::new();
+    let store = setup_store(&config).await;
+
+    let log_filter = format!(
+        // TODO "handle_errors={},api={},warp={}",
+        "api={},warp={}", // TODO
+        config.log_level_api, config.log_level_warp
+    );
+    let logfile =
+        RollingFileAppender::new(Rotation::DAILY, config.log_path_name, config.log_file_name);
+    let (non_blocking_logfile, _guard_logfile) = tracing_appender::non_blocking(logfile);
+    let (non_blocking_stdout, _guard_stdout) = tracing_appender::non_blocking(std::io::stdout());
+    tracing_subscriber::fmt()
+        .with_env_filter(log_filter)
+        .with_writer(non_blocking_logfile.and(non_blocking_stdout))
+        //.with_span_events(tracing_subscriber::fmt::format::FmtSpan::CLOSE)
+        .init();
+
+    let routes = build_routes(store).await;
 
     tracing::info!("Q&A service build ID {}", env!("RUST_WEB_DEV_VERSION"));
     tracing::info!(

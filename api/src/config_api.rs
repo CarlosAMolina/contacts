4c001907c20c673fce5c0377d86dc793f75c09ac
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-08-13 10:48:17 +0200

    Refactor move code to new files config and lib

---
+++ api/src/config_api.rs
@@ -0,0 +1,77 @@
+#![warn(clippy::all)]
+
+use config::Config as Config_;
+
+#[derive(Debug, serde::Deserialize, PartialEq)]
+pub struct Config {
+    pub api_host: [u8; 4],
+    pub api_port: u16,
+    pub database_host: String,
+    pub database_name: String,
+    pub database_password: String,
+    pub database_port: u16,
+    pub database_user: String,
+    pub log_file_name: String,
+    pub log_level_api: String,
+    pub log_level_warp: String,
+    pub log_path_name: String,
+}
+
+impl Config {
+    pub fn new() -> Config {
+        let is_app_in_docker = std::env::var("IS_DOCKER_RUNNING")
+            .ok()
+            .map(|val| val.parse::<bool>())
+            .unwrap_or(Ok(false))
+            .unwrap();
+        let config_file_name = match is_app_in_docker {
+            true => "setup-docker.toml",
+            false => "setup-local.toml",
+        };
+        let config = Config_::builder()
+            .add_source(config::File::with_name(config_file_name))
+            .build()
+            .unwrap();
+        config.try_deserialize::<Config>().unwrap()
+    }
+}
+
+#[cfg(test)]
+mod config_tests {
+    use super::*;
+
+    // As Rust runs test in parallel, we run two tests in the same function
+    // in order to not affect each test when env variables are modified.
+    #[test]
+    fn config_files_are_detected_correctly() {
+        let expected_not_in_docker = Config {
+            api_host: [127, 0, 0, 1],
+            api_port: 3030,
+            database_host: "localhost".to_string(),
+            database_name: "contacts".to_string(),
+            database_password: "pw".to_string(),
+            database_port: 5432,
+            database_user: "postgres".to_string(),
+            log_file_name: "contact-api.log".to_string(),
+            log_level_api: "info".to_string(),
+            log_level_warp: "error".to_string(),
+            log_path_name: "/tmp".to_string(),
+        };
+        let expected_in_docker = Config {
+            api_host: [0, 0, 0, 0],
+            api_port: 3030,
+            database_host: "172.20.0.5".to_string(), // App in localhost
+            database_name: "contacts".to_string(),
+            database_password: "pw".to_string(),
+            database_port: 5432,
+            database_user: "postgres".to_string(),
+            log_file_name: "contact-api.log".to_string(),
+            log_level_api: "info".to_string(),
+            log_level_warp: "error".to_string(),
+            log_path_name: "/tmp".to_string(),
+        };
+        assert_eq!(expected_not_in_docker, Config::new());
+        std::env::set_var("IS_DOCKER_RUNNING", "true");
+        assert_eq!(expected_in_docker, Config::new());
+    }
+}
---
+++ api/src/lib.rs
@@ -0,0 +1,139 @@
+use tracing_appender::rolling::{RollingFileAppender, Rotation};
+use tracing_subscriber::fmt::writer::MakeWriterExt;
+use warp::{http::Method, Filter, Reply};
+
+use handle_errors::return_error;
+
+pub mod config_api; // TODO try use mod instead of mod
+mod routes;
+mod store;
+mod transformers;
+mod types;
+
+pub struct TraceInfo {
+    pub id: uuid::Uuid,
+    pub method: Method,
+    pub path: String,
+    pub referer: String,
+    pub remote_addr: String,
+    pub request_headers: String,
+    pub user_agent: String,
+    pub version: String,
+}
+
+impl TraceInfo {
+    pub fn new(info: &warp::trace::Info<'_>) -> TraceInfo {
+        TraceInfo {
+            id: TraceInfo::get_id(),
+            method: info.method().clone(),
+            path: info.path().to_string(),
+            referer: TraceInfo::get_referer(&info),
+            remote_addr: TraceInfo::get_remote_addr(&info),
+            request_headers: TraceInfo::get_request_headers(&info),
+            user_agent: TraceInfo::get_user_agent(&info),
+            version: TraceInfo::get_version(&info),
+        }
+    }
+
+    fn get_id() -> uuid::Uuid {
+        uuid::Uuid::new_v4()
+    }
+
+    fn get_referer(info: &warp::trace::Info<'_>) -> String {
+        info.referer().unwrap_or("").to_string()
+    }
+
+    fn get_remote_addr(info: &warp::trace::Info<'_>) -> String {
+        let mut remote_addr = "".to_string();
+        if let Some(value) = info.remote_addr() {
+            remote_addr = format!("{:?}", value);
+        }
+        remote_addr
+    }
+
+    fn get_request_headers(info: &warp::trace::Info<'_>) -> String {
+        format!("{:?}", info.request_headers())
+    }
+
+    fn get_user_agent(info: &warp::trace::Info<'_>) -> String {
+        info.user_agent().unwrap_or("").to_string()
+    }
+
+    fn get_version(info: &warp::trace::Info<'_>) -> String {
+        format!("{:?}", info.version())
+    }
+}
+
+// TODO pub async fn setup_store(config: &config::Config) -> Result<store::Store, handle_errors::Error> {
+pub async fn setup_store(config: &config_api::Config) -> store::Store {
+    let db_url = format!(
+        "postgres://{}:{}@{}:{}/{}",
+        config.database_user,
+        config.database_password,
+        config.database_host,
+        config.database_port,
+        config.database_name
+    );
+    store::Store::new(&db_url).await.unwrap()
+    // TODO use .map_err(|e| handle_errors::Error::DatabaseQueryError(e))?;
+}
+
+async fn build_routes(store: store::Store) -> impl Filter<Extract = impl Reply> + Clone {
+    let store_filter = warp::any().map(move || store.clone());
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_header("content-type")
+        .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
+    let get_contacts = warp::get()
+        .and(warp::path("contacts"))
+        .and(warp::path::end())
+        .and(warp::query())
+        .and(store_filter.clone())
+        .and_then(routes::contact::get_contacts)
+        .with(warp::trace(|info| {
+            let info_values = TraceInfo::new(&info);
+            tracing::info_span!(
+                  "get_contacts request",
+                  method = %info_values.method,
+                  path = %info_values.path,
+                  version = %info_values.version,
+                  referer = %info_values.referer,
+                  user_agent = %info_values.user_agent,
+                  remote_addr = %info_values.remote_addr,
+                  request_headers = %info_values.request_headers,
+                  id = %info_values.id,
+            )
+        }));
+    let get_contact_by_id = warp::get()
+        .and(warp::path("contacts"))
+        .and(warp::path::param::<i32>())
+        .and(warp::path::end())
+        .and(store_filter.clone())
+        .and_then(routes::contact::get_contact_by_id)
+        .with(warp::trace(|info| {
+            let info_values = TraceInfo::new(&info);
+            tracing::info_span!(
+                  "get_contact_by_id request",
+                  method = %info_values.method,
+                  path = %info_values.path,
+                  version = %info_values.version,
+                  referer = %info_values.referer,
+                  user_agent = %info_values.user_agent,
+                  remote_addr = %info_values.remote_addr,
+                  request_headers = %info_values.request_headers,
+                  id = %info_values.id,
+            )
+        }));
+    get_contacts
+        .or(get_contact_by_id)
+        .with(cors)
+        .with(warp::trace::request())
+        .recover(return_error)
+}
+
+pub async fn run(config: config_api::Config, store: store::Store) {
+    let routes = build_routes(store).await;
+    warp::serve(routes)
+        .run((config.api_host, config.api_port))
+        .await;
+}

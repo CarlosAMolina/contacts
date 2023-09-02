a3eb505be62a0149be8468fffed6dc0025762498
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-09-02 23:02:54 +0200

    Refactor sort db types

--- api/src/types/database.rs
+++ api/src/types/database.rs
@@ -1,43 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, PartialEq, Serialize)]
-pub struct Category {
-    pub id: i32,
-    pub category: String,
-}
-
-#[derive(Debug, Deserialize, PartialEq, Serialize)]
-pub struct UserCategory {
-    pub id_user: i32,
-    pub id_category: i64,
-}
-
-#[derive(Debug, Deserialize, PartialEq, Serialize)]
-pub struct Phone {
-    pub id_user: i32,
-    pub phone: i64,
-    pub description: Option<String>,
-}
-
-#[derive(Debug, Deserialize, PartialEq, Serialize)]
-pub struct User {
-    pub id: i32,
-    pub name: String,
-    pub surname: Option<String>,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct NewUser {
-    pub name: String,
-    pub surname: Option<String>,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct Nickname {
-    pub id_user: i32,
-    pub nickname: String,
-}
-
 #[derive(Clone, Debug, Serialize)]
 pub struct AllData {
     pub user_id: i32,
@@ -54,4 +16,42 @@ pub struct AllData {
     pub twitter_handle: Option<String>,
     pub instagram_handle: Option<String>,
     pub note: Option<String>,
+}
+
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
+pub struct Category {
+    pub id: i32,
+    pub category: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct NewUser {
+    pub name: String,
+    pub surname: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Nickname {
+    pub id_user: i32,
+    pub nickname: String,
+}
+
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
+pub struct Phone {
+    pub id_user: i32,
+    pub phone: i64,
+    pub description: Option<String>,
+}
+
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
+pub struct User {
+    pub id: i32,
+    pub name: String,
+    pub surname: Option<String>,
+}
+
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
+pub struct UserCategory {
+    pub id_user: i32,
+    pub id_category: i64,
 }

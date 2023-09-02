86282ebc9300f56d411a7f67e34d7049a2037385
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-09-02 18:11:20 +0200

    Refactor remove else

--- api/src/routes/contact.rs
+++ api/src/routes/contact.rs
@@ -32,27 +32,26 @@ pub async fn add_contact (
     let nickname_db = store.add_nickname(nickname).await;
     if let Err(e) = nickname_db {
         return Err(warp::reject::custom(e));
-    } else {
-        // TODO improve previous if-else
-        // TODO user previous types values
-        let nickname_db_ok = nickname_db.unwrap();
-        let contact = contact_types::Contact {
-            user_id: user_db_ok.id,
-            user_name: user_db_ok.name,
-            user_surname: user_db_ok.surname,
-            nicknames: vec![nickname_db_ok.nickname],
-            phones: vec![],
-            categories: vec![],
-            addresses: vec![],
-            emails: vec![],
-            urls: vec![],
-            facebook_urls: vec![],
-            twitter_handles: vec![],
-            instagram_handles: vec![],
-            note: None,
-        };
-        return Ok(warp::reply::json(&contact));
     }
+    // TODO improve previous if-else
+    // TODO user previous types values
+    let nickname_db_ok = nickname_db.unwrap();
+    let contact = contact_types::Contact {
+        user_id: user_db_ok.id,
+        user_name: user_db_ok.name,
+        user_surname: user_db_ok.surname,
+        nicknames: vec![nickname_db_ok.nickname],
+        phones: vec![],
+        categories: vec![],
+        addresses: vec![],
+        emails: vec![],
+        urls: vec![],
+        facebook_urls: vec![],
+        twitter_handles: vec![],
+        instagram_handles: vec![],
+        note: None,
+    };
+    return Ok(warp::reply::json(&contact));
 }
 
 // TODO rm pub

2b5f3fc2c6a010acb73c995e2949216cb913a0de
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-09-05 21:18:14 +0200

    Improve sql syntax

--- api/src/store.rs
+++ api/src/store.rs
@@ -366,66 +366,18 @@ SELECT
   instagram.handle AS instagram_handle,
   notes.note
 FROM
-  (
-    (
-      (
-        (
-          (
-            (
-              (
-                (
-                  (
-                    (
-                      (
-                        contacts.users
-                        LEFT JOIN contacts.addresses ON (
-                          (users.id = addresses.id_user)
-                        )
-                      )
-                      LEFT JOIN contacts.users_categories ON (
-                        (
-                          users.id = users_categories.id_user
-                        )
-                      )
-                    )
-                    LEFT JOIN contacts.categories ON (
-                      (
-                        users_categories.id_category = categories.id
-                      )
-                    )
-                  )
-                  LEFT JOIN contacts.emails ON (
-                    (users.id = emails.id_user)
-                  )
-                )
-                LEFT JOIN contacts.facebook ON (
-                  (users.id = facebook.id_user)
-                )
-              )
-              LEFT JOIN contacts.instagram ON (
-                (users.id = instagram.id_user)
-              )
-            )
-            LEFT JOIN contacts.nicknames ON (
-              (users.id = nicknames.id_user)
-            )
-          )
-          LEFT JOIN contacts.notes ON (
-            (users.id = notes.id_user)
-          )
-        )
-        LEFT JOIN contacts.phones ON (
-          (users.id = phones.id_user)
-        )
-      )
-      LEFT JOIN contacts.twitter ON (
-        (users.id = twitter.id_user)
-      )
-    )
-    LEFT JOIN contacts.urls ON (
-      (users.id = urls.id_user)
-    )
-  )
+contacts.users
+LEFT JOIN contacts.addresses ON users.id = addresses.id_user
+LEFT JOIN contacts.users_categories ON users.id = users_categories.id_user
+LEFT JOIN contacts.categories ON users_categories.id_category = categories.id
+LEFT JOIN contacts.emails ON users.id = emails.id_user
+LEFT JOIN contacts.facebook ON users.id = facebook.id_user
+LEFT JOIN contacts.instagram ON users.id = instagram.id_user
+LEFT JOIN contacts.nicknames ON users.id = nicknames.id_user
+LEFT JOIN contacts.notes ON users.id = notes.id_user
+LEFT JOIN contacts.phones ON users.id = phones.id_user
+LEFT JOIN contacts.twitter ON users.id = twitter.id_user
+LEFT JOIN contacts.urls ON users.id = urls.id_user
 where
   users.id in (
     SELECT

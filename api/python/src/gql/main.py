a0fa8013e0486d173ac0454bf1006b6b0507c601
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-04-16 02:30:25 +0200

    Change methods to static

--- api/python/src/gql/main.py
+++ api/python/src/gql/main.py
@@ -21,15 +21,13 @@ class Query(ObjectType):
     user = Field(UserType, user_id=Int())
     users_by_min_age = List(UserType, min_age=Int())
 
-    # @staticmethod
-    # def resolve_user(root, info, user_id=Int()) -> dict:
-    def resolve_user(self, info, user_id=Int()) -> dict:
+    @staticmethod
+    def resolve_user(root, info, user_id=Int()) -> dict:
         matched_users = [user for user in users if user["id"] == user_id]
         return matched_users[0] if matched_users else None
 
-    # @staticmethod
-    # def resolve_users_by_min_age(root, info, min_age=Int()) -> tp.List[dict]:
-    def resolve_users_by_min_age(self, info, min_age=Int()) -> tp.List[dict]:
+    @staticmethod
+    def resolve_users_by_min_age(root, info, min_age=Int()) -> tp.List[dict]:
         return [user for user in users if user["age"] >= min_age]
 
 

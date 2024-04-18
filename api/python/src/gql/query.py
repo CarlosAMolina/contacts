adb3f5607d2686e70c7a097c8399f819389227be
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-04-18 05:20:23 +0200

    Refactor extract to file

---
+++ api/python/src/gql/query.py
@@ -0,0 +1,30 @@
+import typing as tp
+
+from graphene import Field
+from graphene import List
+from graphene import ObjectType
+from graphene import Int
+
+from src.db.database import db_session
+from src.db.models import EmailModel
+from src.db.models import UserModel
+from src.gql.types import EmailObject
+from src.gql.types import UserObject
+
+
+class Query(ObjectType):
+    email = Field(EmailObject, email_id=Int())
+    user = Field(UserObject, user_id=Int())
+    users_by_min_age = List(UserObject, min_age=Int())
+
+    @staticmethod
+    def resolve_email(root, info, email_id=Int()) -> tp.Optional[EmailModel]:
+        return db_session.query(EmailModel).filter(EmailModel.id == email_id).first()
+
+    @staticmethod
+    def resolve_user(root, info, user_id=Int()) -> tp.Optional[UserModel]:
+        return db_session.query(UserModel).filter(UserModel.id == user_id).first()
+
+    @staticmethod
+    def resolve_users_by_min_age(root, info, min_age=Int()) -> tp.List[UserModel]:
+        return db_session.query(UserModel).filter(UserModel.age >= min_age).all()

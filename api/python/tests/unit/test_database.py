1f48889cd564268100df9e18deb3edb3f81306ce
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-04-28 10:04:02 +0200

    Change how to get env, add assertion

--- api/python/tests/unit/test_database.py
+++ api/python/tests/unit/test_database.py
@@ -9,5 +9,6 @@ from src.db import database
 class TestFunction_init_db(unittest.TestCase):
     def test_database_is_created(self):
         database.prepare_db()
-        db_path_name = os.environ["DB_PATH_NAME"]
+        db_path_name = os.getenv("DB_PATH_NAME")
+        self.assertIsNotNone(db_path_name)
         self.assertTrue(Path(db_path_name).is_file())

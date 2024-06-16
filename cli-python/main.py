712d2378db30ab843518dddd7d7992afce24e7f3
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-06-16 12:47:57 +0200

    Refactor extract function

--- cli-python/main.py
+++ cli-python/main.py
@@ -6,24 +6,32 @@ class TerminationId:
     SUCCESSFUL = 0
 
 
-if (args_count := len(sys.argv)) == 1:
+def run_iteractive():
     print("Starting iteractive mode")
     print("Welcome to the contacts CLI!")
     while True:
         print("What do you want to search?")
         search_term = input()
         print("Searching term", search_term)
-elif args_count == 2:
-    search_term = sys.argv[1]
-    print("Searching term", search_term)
-elif args_count == 3:
-    option = sys.argv[1]
-    if option != "-i":
-        print("Invalid option", option)
-        raise SystemExit(TerminationId.ABNORMAL)
+
+
+def get_what_to_run_parsing_args():
+    if (args_count := len(sys.argv)) == 1:
+        run_iteractive()
+    elif args_count == 2:
+        search_term = sys.argv[1]
+        print("Searching term", search_term)
+    elif args_count == 3:
+        option = sys.argv[1]
+        if option != "-i":
+            print("Invalid option", option)
+            raise SystemExit(TerminationId.ABNORMAL)
+        else:
+            search_id = sys.argv[2]
+            print("Start displaying ID", search_id)
     else:
-        search_id = sys.argv[2]
-        print("Start displaying ID", search_id)
-else:
-    print("Unmanaged situation")
-    raise SystemExit(TerminationId.ABNORMAL)
+        print("Unmanaged situation")
+        raise SystemExit(TerminationId.ABNORMAL)
+
+
+get_what_to_run_parsing_args()

f1bb7f737ecdf77cd800e76584e760f335fabb99
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-06-16 12:44:09 +0200

    Add api python sys arg

---
+++ cli-python/main.py
@@ -0,0 +1,29 @@
+import sys
+
+
+class TerminationId:
+    ABNORMAL = 1
+    SUCCESSFUL = 0
+
+
+if (args_count := len(sys.argv)) == 1:
+    print("Starting iteractive mode")
+    print("Welcome to the contacts CLI!")
+    while True:
+        print("What do you want to search?")
+        search_term = input()
+        print("Searching term", search_term)
+elif args_count == 2:
+    search_term = sys.argv[1]
+    print("Searching term", search_term)
+elif args_count == 3:
+    option = sys.argv[1]
+    if option != "-i":
+        print("Invalid option", option)
+        raise SystemExit(TerminationId.ABNORMAL)
+    else:
+        search_id = sys.argv[2]
+        print("Start displaying ID", search_id)
+else:
+    print("Unmanaged situation")
+    raise SystemExit(TerminationId.ABNORMAL)

257a98a0fe2be93deabdbb901d1cc907d0273d69
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-06-16 11:13:29 +0200

    Improve flask host env

--- api/python/main.py
+++ api/python/main.py
@@ -34,10 +34,5 @@ def shutdown_session(exception=None):
 if __name__ == "__main__":
     print("GraphiQL URL: http://127.0.0.1:5000/graphql")
     # https://stackoverflow.com/a/30329547
-    if os.getenv("IS_DOCKER"):
-        # To detect IP used by the Docker container, when the container is running,
-        # execute `docker network inspect bridge | grep IPv4Address`
-        # https://stackoverflow.com/a/58138250
-        app.run(host="172.17.0.2")
-    else:
-        app.run()
+    host = os.getenv("FLASK_HOST")
+    app.run(host=host)

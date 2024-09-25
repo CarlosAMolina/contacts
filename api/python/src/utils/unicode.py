28d6972d1d9ccd47f0dbfae7c8480def98ec850c
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-09-25 23:15:14 +0200

    refactor (api python): move code

--- api/python/src/utils/unicode.py
+++ api/python/src/utils/unicode.py
@@ -1,19 +1,6 @@
 from sqlalchemy import Column
 from sqlalchemy import func
 
-_ACCENT_TO_NO_ACCENT_MAP = {
-    "á": "a",
-    "Á": "A",
-    "é": "e",
-    "É": "E",
-    "í": "i",
-    "Í": "I",
-    "ó": "o",
-    "Ó": "O",
-    "ú": "u",
-    "Ú": "U",
-}
-
 
 def get_column_unicode(column: Column[str]) -> Column[str]:
     result = column
@@ -27,3 +14,17 @@ def get_string_unicode(string: str) -> str:
     for old, new in _ACCENT_TO_NO_ACCENT_MAP.items():
         result = result.replace(old, new)
     return result
+
+
+_ACCENT_TO_NO_ACCENT_MAP = {
+    "á": "a",
+    "Á": "A",
+    "é": "e",
+    "É": "E",
+    "í": "i",
+    "Í": "I",
+    "ó": "o",
+    "Ó": "O",
+    "ú": "u",
+    "Ú": "U",
+}

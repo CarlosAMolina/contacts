2dd960a80d94156ce75de43389c451e61c493e72
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-08-04 21:35:51 +0200

    feat(export): add assert no lost ID

--- cli-python/src/export.py
+++ cli-python/src/export.py
@@ -5,4 +5,25 @@ class Export:
     def export_json(self):
         print("Exporting JSON")
         db_json = AllSearch().get_all_db()
-        print(db_json)  # TODO rm
+        json = _Json(db_json)
+        self._assert_all_ids_exist(json.get_ids())
+        #print(db_json)  # TODO rm
+
+    def _assert_all_ids_exist(self, ids: list[int]):
+        expected_value = 1
+        for id_ in ids:
+            if not id_ == expected_value:
+                raise ValueError(f"Lost ID: {id_}")
+            expected_value += 1
+
+
+class _Json:
+    def __init__(self, json: dict):
+        self._json = json
+
+    def get_ids(self) -> list[int]:
+        return sorted([user["id"] for user in self._get_users()])
+
+    def _get_users(self) -> list[dict]:
+        return self._json["data"]["usersWithTerm"]
+
